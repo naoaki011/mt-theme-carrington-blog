dd378483add3cd73007f9bd9c778bd51fcf8c491
Mike R T <mikerthomsen@gmail.com>
2010-06-10 21:57:23 -0400

    Fixes

--- plugins/CarringtonBlogTheme/static/js/jquery.reply.js
+++ plugins/CarringtonBlogTheme/static/js/jquery.reply.js
@@ -3,14 +3,27 @@
         var commentForm = jQuery("form.comment-form");
 	findParentComment = function(e) { return e.closest('.comment'); }
         var clicked = Array();
-	alert('sdafasdf');
+	var replyField = jQuery("#reply-label", commentForm);
+	var cancelReply = jQuery("#cancel-comment-reply", commentForm);
+	var oldReplyText = replyField.text();
+	var parentIdInput = jQuery("#comment-parent-id", commentForm);
+
+	cancelReply.click(function() {
+		replyField.text(oldReplyText);
+		jQuery(this).hide();
+		parentIdInput.val('');
+		return false;
+	});
+
         var onReplyClick = function() {
 		var replyLink = jQuery(this);
 		var pid_e = findParentComment( jQuery(this) );
 		var id = pid_e.attr('id');
 		var idNumber = id.substring(id.indexOf('-')+1, id.length);
-		jQuery("#comment-parent-id", commentForm).val(idNumber);
-		alert(idNumber);
+		var author = jQuery("a.url", pid_e).text();
+		parentIdInput.val(idNumber);
+		replyField.html("Replying to " + author );
+		cancelReply.show();
         };
         return this.each(function() {
             jQuery(this).click( onReplyClick );
